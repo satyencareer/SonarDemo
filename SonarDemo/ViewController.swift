6368729157f9b4c4c89e8db48ab043434f53a295
satyendra chauhan <>
2019-04-25 10:15:46 +0300

    sonar source all changes

---
+++ SonarDemo/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  SonarDemo
+//
+//  Created by satyendra chauhan on 4/22/19.
+//  Copyright © 2019 satyendra chauhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func mastFunction () -> String {
+        var noProb = "test one"
+        
+        return noProb
+    }
+    
+    func secretCode  ()       {
+        var dis = ""
+        var  a = 10
+        var b =  20
+        
+        if a > b  {
+            print("Hello a")
+        } else {
+            print("Hello b")
+        }
+        
+        var c = 1000
+        var d = 10
+        
+        if  d > c  {
+            print   ("no")
+        }
+        let make = 20
+        var maked = 20
+
+        var string = "here we need to add 20 line of code otherwise it will be ignored"
+        secretCode ()
+        secretCode()
+        secretCode()
+        secretCode()
+        secretCode()
+        secretCode()
+        secretCode()
+        secretCode ()
+        secretCode()
+        secretCode()
+        secretCode ()
+        secretCode ()
+    }
+    
+    func kasdfhksdsdjfhskafhskfhskafhksahfkashfhasfhaskfhsaklfhlsakfdhksafhksafhkasfhksaf(){
+        
+    }
+    
+    func simpleMax < T: Comparable >(x: T, _ y: T) -> T {
+        if x < y {
+            return y
+        }
+        return x
+    }
+
+}
+
